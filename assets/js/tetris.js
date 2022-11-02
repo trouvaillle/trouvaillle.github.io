2c92505f5ee707f2ac9c7b2f64071a8f6afe28c0
trouvaillle <66727698+trouvaillle@users.noreply.github.com>
2022-11-03 01:57:11 +0900

    disable sfx on iOS

--- assets/js/tetris.js
+++ assets/js/tetris.js
@@ -63,6 +63,9 @@ window.onload = () => {
                 !(type in this.sfxController)) {
                 return;
             }
+            if (getMobileOperatingSystem() == "iOS") {
+                return;
+            }
             if (this.audioContext != null && this.sfxController[type].source != null) {
                 this.sfxController[type].source.noteOn(0);
                 this.sfxController[type].source.connect(this.audioContext.destination);
@@ -1193,5 +1196,25 @@ window.onload = () => {
 
     function convertRemToPixels(rem) {
         return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
+    }
+
+    function getMobileOperatingSystem() {
+        var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+        // Windows Phone must come first because its UA also contains "Android"
+        if (/windows phone/i.test(userAgent)) {
+            return "Windows Phone";
+        }
+
+        if (/android/i.test(userAgent)) {
+            return "Android";
+        }
+
+        // iOS detection from: http://stackoverflow.com/a/9039885/177710
+        if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+            return "iOS";
+        }
+
+        return "unknown";
     }
 };
