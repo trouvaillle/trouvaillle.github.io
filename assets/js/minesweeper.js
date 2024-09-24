3b354f8c1f383f298af1f1cc81ecda8dd48a7175
USER <user@AL02003994.local>
2024-09-24 20:47:39 +0900

    feat: area click support

--- assets/js/minesweeper.js
+++ assets/js/minesweeper.js
@@ -1,6 +1,13 @@
 window.onload = () => {
-  const {x, y, mines} = {x: 10, y: 10, mines: 10};
-  let map = []; // 10 - mine, 11 - bombed mine, 12 - flag, 13 - mined flag, 14 - question, 15 - mined question
+  const ITEM_MINE = 10;
+  const ITEM_BOMBED_MINE = 11;
+  const ITEM_FLAG = 12;
+  const ITEM_FLAG_WITH_MINE = 13;
+  const ITEM_QUESTION = 14;
+  const ITEM_QUESTION_WITH_MINE = 15;
+
+  let {x, y, mines} = {x: 10, y: 10, mines: 10};
+  let map = []; // 10 - mine, 11 - bombed mine, 12 - flag, 13 - flag with mine, 14 - question, 15 - question with mine
   let mineCoords = [];
   let items = [];
   let gameover = false;
@@ -10,7 +17,6 @@ window.onload = () => {
   const container =  document.querySelector('#container');
   const board =  document.querySelector('#board');
   const smiley =  document.querySelector('#smiley');
-  const youwin =  document.querySelector('#youwin');
   
   initMap();
   createBoard();
@@ -23,6 +29,9 @@ window.onload = () => {
     alreadyTaken = new Set();
     left = mines;
     gameover = false;
+    win = false;
+    score = 0;
+    notClicked = x * y;
 
     while (left > 0) {
         const maxNumber = x * y;
@@ -43,15 +52,12 @@ window.onload = () => {
     map = [...new Array(y).keys()].map(_ => [...new Array(x).keys()].map(_ => 0));
 
     for (const [a, b] of mineCoords) {
-        map[b][a] = 10;
+        map[b][a] = ITEM_MINE;
     }
-
-    notClicked = x * y;
   }
 
   function adjustSize() {
     const cellWidth = Math.min(container.clientWidth / x, container.clientHeight / y);
-    console.log(cellWidth);
     board.setAttribute('style', `width: ${cellWidth * x}px; height: ${cellWidth * y}px; font-size: ${cellWidth * 0.25}px`);
   }
 
@@ -69,8 +75,10 @@ window.onload = () => {
             let thisItemDown = false;
             const itemMouseDown = (item) => (evt) => {
                 evt.preventDefault();
-                if (map[j][i] === 0 || map[j][i] === 10 || (map[j][i] >= 12 && map[j][i] <= 15)) {
-                    item.classList.add('mousedown');
+                if (map[j][i] >= 0 && map[j][i] <= ITEM_MINE || (map[j][i] >= 12 && map[j][i] <= 15)) {
+                    if (evt.button === 0) {
+                        item.classList.add('mousedown');
+                    }
                     thisItemDown = true;
                     smiley.innerHTML = '😮';
                 }
@@ -87,12 +95,15 @@ window.onload = () => {
                 if (resetGame()) {
                     return;
                 }
-                if (map[j][i] === 0 || map[j][i] === 10 || (map[j][i] >= 12 && map[j][i] <= 15)) {
-                    item.classList.remove('mousedown');
+                if (map[j][i] >= 0 && map[j][i] <= ITEM_MINE || (map[j][i] >= 12 && map[j][i] <= 15)) {
+                    if (!(map[j][i] > 0 && map[j][i] < ITEM_MINE)) {
+                        item.classList.remove('mousedown');
+                    }
                     if (thisItemDown) {
                         if (evt.button === 0) {
-                            itemClicked(i, j);
+                            clickItem(i, j);
                             checkWin();
+                            render();
                         } else if (evt.button === 2) {
                             if (map[j][i] === 0) {
                                 map[j][i] = 12;
@@ -116,7 +127,7 @@ window.onload = () => {
             };
             const itemMouseLeave = (item) => (evt) => {
                 evt.preventDefault();
-                if (map[j][i] === 0 || (!gameover && map[j][i] === 10) || (map[j][i] >= 12 && map[j][i] <= 15)) {
+                if (map[j][i] === 0 || (!gameover && map[j][i] === ITEM_MINE) || (map[j][i] >= 12 && map[j][i] <= 15)) {
                     item.classList.remove('mousedown');
                     thisItemDown = false;
                     setSmiley();
@@ -143,10 +154,6 @@ window.onload = () => {
         board.appendChild(row);
         items.push(itemsRow);
       }
-
-      youwin.addEventListener('click', () => {
-        resetGame();
-      });
   }
 
   function resetGame() {
@@ -159,21 +166,39 @@ window.onload = () => {
     }
   }
 
-  function itemClicked(i, j) {
-    if (map[j][i] === 0 || map[j][i] === 14) {
+  function clickItem(i, j, prevent) {
+    if (!(i >= 0 && i < x && j >= 0 && j < y)) {
+        return;
+    }
+    if (map[j][i] === 0 || map[j][i] === ITEM_QUESTION) {
         const num = getSurroundedMines(i, j);
         map[j][i] = num + 1;
         --notClicked;
         if (num == 0) {
             for (let a = 0; a < 3; ++a) {
                 for (let b = 0; b < 3; ++b) {
-                    checkAndClick(i + a - 1, j + b -1);
+                    if (!(a === 1 && b === 1)) {
+                        checkAndClick(i + a - 1, j + b -1);
+                    }
                 }
             }
         }
-    } else if (map[j][i] === 10 || map[j][i] === 15) {
-        map[j][i] = 11;
+    } else if (map[j][i] === ITEM_MINE || map[j][i] === ITEM_QUESTION_WITH_MINE) {
+        map[j][i] = ITEM_BOMBED_MINE;
         gameover = true;
+    } else if (map[j][i] > 0 && map[j][i] < ITEM_MINE) {
+        if (prevent) return;
+        const surroundedMines = getSurroundedMines(i, j);
+        const surroundedConfirms = getSurroundedConfirms(i, j);
+        if (surroundedMines === surroundedConfirms) {
+            for (let a = 0; a < 3; ++a) {
+                for (let b = 0; b < 3; ++b) {
+                    if (!(a === 1 && b === 1)) {
+                        clickItem(i + a - 1, j + b -1, true);
+                    }
+                }
+            }
+        }
     }
     render();
   }
@@ -181,7 +206,7 @@ window.onload = () => {
   function checkAndClick(i, j) {
     if (i >= 0 && i < x && j >= 0 && j < y) {
         if (map[j][i] === 0) {
-            itemClicked(i, j);
+            clickItem(i, j);
         }
     }
   }
@@ -201,7 +226,7 @@ window.onload = () => {
   function render() {
     for (let j = 0; j < y; ++j) {
         for (let i = 0; i < x; ++i) {
-            if (map[j][i] === 11 || (gameover && map[j][i] === 10)) {
+            if (map[j][i] === ITEM_BOMBED_MINE || (gameover && map[j][i] === ITEM_MINE)) {
                 items[j][i].classList.remove('red');
                 items[j][i].classList.remove('flag');
                 items[j][i].classList.remove('question');
@@ -210,14 +235,14 @@ window.onload = () => {
                     items[j][i].classList.add('red');
                 }
                 items[j][i].classList.add('mousedown');
-            } else if (map[j][i] > 0 && map[j][i] < 10) {
+            } else if (map[j][i] > 0 && map[j][i] < ITEM_MINE) {
                 items[j][i].classList.remove('red');
                 items[j][i].classList.remove('mine');
                 items[j][i].classList.remove('flag');
                 items[j][i].classList.remove('question');
                 items[j][i].classList.add(`number-${map[j][i] - 1}`);
                 items[j][i].classList.add('mousedown');
-            } else if (map[j][i] === 0 || map[j][i] === 10) {
+            } else if (map[j][i] === 0 || map[j][i] === ITEM_MINE) {
                 items[j][i].classList.remove('red');
                 items[j][i].classList.remove('mine');
                 items[j][i].classList.remove('flag');
@@ -226,22 +251,18 @@ window.onload = () => {
                 for (let k = 1; k < 9; ++k) {
                     items[j][i].classList.remove(`number-${k}`);
                 }
-            } else if (map[j][i] === 12 || map[j][i] === 13) {
+            } else if (map[j][i] === ITEM_FLAG || map[j][i] === ITEM_FLAG_WITH_MINE) {
                 items[j][i].classList.remove('red');
                 items[j][i].classList.remove('question');
                 items[j][i].classList.add('flag');
                 items[j][i].classList.remove('mousedown');
-            } else if (map[j][i] === 14 || map[j][i] === 15) {
+            } else if (map[j][i] === ITEM_QUESTION || map[j][i] === ITEM_QUESTION_WITH_MINE) {
                 items[j][i].classList.remove('red');
                 items[j][i].classList.remove('flag');
                 items[j][i].classList.add('question');
                 items[j][i].classList.remove('mousedown');
             }
         }
-    }
-
-    if (!gameover || !win) {
-        youwin.removeAttribute('style');
     }
 
     setSmiley();
@@ -252,13 +273,24 @@ window.onload = () => {
   }
 
   function getSurroundedMines(a, b) {
+    const bombs = [ITEM_MINE, ITEM_BOMBED_MINE, ITEM_FLAG_WITH_MINE, ITEM_QUESTION_WITH_MINE];
+    return getSurroundedItems(a, b, bombs);
+  }
+
+  function getSurroundedConfirms(a, b) {
+    const confirms = [ITEM_FLAG, ITEM_FLAG_WITH_MINE];
+    return getSurroundedItems(a, b, confirms);
+  }
+
+  function getSurroundedItems(a, b, types) {
     let result = 0;
     for (let j = 0; j < 3; ++j) {
         for (let i = 0; i < 3; ++i) {
+            if (i === 1 && j === 1) continue;
             const c = a + i - 1;
             const d = b + j - 1;
             if (c >= 0 && c < x && d >= 0 && d < y) {
-                if (map[d][c] === 10 || map[d][c] === 11 || map[d][c] === 13 || map[d][c] === 15) {
+                if (types.includes(map[d][c])) {
                     ++result;
                 }
             }
@@ -271,7 +303,13 @@ window.onload = () => {
     if (notClicked === mines) {
         gameover = true;
         win = true;
-        youwin.setAttribute('style', 'visibility: visible;');
+        for (let j = 0; j < y; ++j) {
+            for (let i = 0; i < x; ++i) {
+                if (map[j][i] === ITEM_MINE) {
+                    map[j][i] = ITEM_FLAG;
+                }
+            }
+        }
     }
   }
 };
